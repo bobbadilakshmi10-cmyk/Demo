31bd01acdbea1284218e16b53e4dcaa95d9a2ff8
bobbadilakshmi10-cmyk <bobbadilakshmi10@gmail.com>
2026-01-10 14:06:45 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,11 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print(10+2)
+12
+>>> rint(10+3)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    rint(10+3)
+NameError: name 'rint' is not defined. Did you mean: 'print'?
+>>> print(10+3)
+13
