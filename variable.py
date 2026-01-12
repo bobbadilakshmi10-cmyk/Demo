8e568b29bdfb7c5512d4f2e6047219bb4b8bbcf8
bobbadilakshmi10-cmyk <bobbadilakshmi10@gmail.com>
2026-01-12 10:59:18 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,142 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=10
+print(a)
+10
+print(b)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    print(b)
+NameError: name 'b' is not defined
+b=20
+print(b)
+20
+c=50
+print(c)
+50
+a12
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    a12
+NameError: name 'a12' is not defined
+a=12
+print(a)
+12
+a12=34
+print(a12)
+34
+a123456(200)
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    a123456(200)
+NameError: name 'a123456' is not defined
+a123456=200
+print(a123456)
+200
+2=12
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a23=100
+print(100)
+100
+name="lakshmi"
+print(name)
+lakshmi
+print("name")
+name
+city=vij
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    city=vij
+NameError: name 'vij' is not defined
+print("vij")
+vij
+fname="lakshmi"
+lname="b"
+print(fname+lname)
+lakshmib
+print(fname+" "+lname)
+lakshmi b
+print(fname,lname)
+lakshmi b
+country="india"
+print(country)
+india
+if=2
+SyntaxError: invalid syntax
+else=50
+SyntaxError: invalid syntax
+7=20
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a=50
+_a=100
+print(_a)
+100
+@a=50
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+_=40
+print(_)
+40
+maild="lakshmi@gmail.com"
+print(maild)
+lakshmi@gmail.com
+100
+100
+
+a=10
+b=20
+print(a+b)
+30
+a,b=3,7
+print(a+b)
+10
+a=4,b=10
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=4;b=5
+print(a+b)
+9
+a,b,c=100,100,100
+print(a,b,c)
+100 100 100
+a,b,c=100
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    a,b,c=100
+TypeError: cannot unpack non-iterable int object
+>>> a=b=c=100
+>>> print(a,b,c)
+100 100 100
+>>> fname,lname="lakshmi","b"
+>>> print(fname+lname)
+lakshmib
+>>> print(fname,lname)
+lakshmi b
+>>> print(fname+""+lname)
+lakshmib
+>>> f name="lakshmi"
+SyntaxError: invalid syntax
+>>> f_name="lakshmi"
+>>> print(f_name)
+lakshmi
+>>> fname="lakshmi"
+>>> print(fname)
+lakshmi
+>>> a=4
+>>> print(a)
+4
+>>> del(a)
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#68>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: '_a'?
+>>> city="vij"
+>>> print(city)
+vij
+>>> City="vij"
+>>> print(City)
+vij
+>>> CITY="vij)
+SyntaxError: unterminated string literal (detected at line 1)
+>>> CITY="vij"
+>>> print(CITY)
+vij
