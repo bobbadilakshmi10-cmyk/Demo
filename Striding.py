c2441a4c62bf42949a09c4ed45c6cb30b3a02ffe
bobbadilakshmi10-cmyk <bobbadilakshmi10@gmail.com>
2026-01-20 11:10:12 +0530

    Add files via upload

---
+++ Striding.py
@@ -0,0 +1,27 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a="Machine Learning"
+>>> a[2:7:2]
+'cie'
+>>> a[2:14:3]
+'cnLr'
+>>> a[3:15:2]
+'hn eri'
+>>> a[1:11:4]
+'ane'
+>>> #Negative Striding
+>>> a="Python course"
+>>> a[-1:-9:-3]
+'eu '
+>>> a[-2:-12:-4]
+'sch'
+>>> a[7:3:1]
+''
+>>> a[3:7:1]
+'hon '
+>>> a[3:-9:-1]
+''
+>>> a[-3:-9:-1]
+'ruoc n'
+>>> a[::-1]
+'esruoc nohtyP'
