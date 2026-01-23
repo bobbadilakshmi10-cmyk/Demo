66687e8806b366f2b22367203e7f4c705cd2e2b2
bobbadilakshmi10-cmyk <bobbadilakshmi10@gmail.com>
2026-01-23 10:18:42 +0530

    Add files via upload

---
+++ list task1.py
@@ -0,0 +1,9 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=[9,1,5,2,8,4,6,3,7,0]
+>>> a=[a.index(3),a.index(6),a.index(8),a.index(2),a.index(9),a.index(0),a.index(7),a.index(4),a.index(5),a.index(1)]
+... 
+>>> a
+[7, 6, 4, 3, 0, 9, 8, 5, 2, 1]
+>>> a
+[7, 6, 4, 3, 0, 9, 8, 5, 2, 1]
