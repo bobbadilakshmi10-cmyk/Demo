356a0d8b87eb147999199b50ed159694e3f540dd
bobbadilakshmi10-cmyk <bobbadilakshmi10@gmail.com>
2026-01-23 20:13:28 +0530

    Add files via upload

---
+++ run time input.py
@@ -0,0 +1,67 @@
+'''a=10
+b=20
+print(a+b)'''
+
+'''a=10
+b=20
+c=a+b
+print(c)'''
+
+'''print(a+b)'''
+
+
+# run_time_input()
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+print(a+b)'''
+
+'''a=str(input("enter the a value"))
+b=str(input("enter the b value"))
+print(a+b)'''
+
+'''a=(input("enter the a value"))
+b=(input("enter the b value"))
+print(a+b)'''
+
+'''a=float(input("enter the a value"))
+b=float(input("enter the b value"))
+print(a+b)'''
+
+'''fname=(input("enter the a value"))
+lname=(input("enter the b value"))
+print((fname+" "+lname).title())'''
+
+'''a=complex(input("enter the a value"))
+b=complex(input("enter the b value"))
+print(a+b)'''
+
+'''a=boolen(input("enter the a value"))
+b=boolen(input("enter the b value"))
+print(a+b)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+print(input("chose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=(input("a value"))
+b=(input("b value"))
+print(input("chose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+a=int(input())
+b=int(input())
+print(a+b)
+
+name=input()
+print(name)
+
+
+
+
+
+
