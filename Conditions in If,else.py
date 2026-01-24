7ad8ee5020e9bfe132e91c606c19ab79f2f88230
bobbadilakshmi10-cmyk <bobbadilakshmi10@gmail.com>
2026-01-24 11:02:56 +0530

    Add files via upload

---
+++ Conditions in If,else.py
@@ -0,0 +1,297 @@
+#conditions
+#if conditions using comparision operators
+#<,>,<=,>=,!=,==
+
+'''a=10
+b=20
+if a<b:
+    print("true")'''
+
+
+'''a=10
+b=20
+if a>b:
+    print("true")'''
+
+'''a=10
+b=20
+if a<=b:
+    print("true")'''
+
+'''a=10
+b=20
+if a>=b:
+    print("False")'''
+
+'''a=10
+b=20
+if a!=b:
+    print("not equal")'''
+
+
+'''a=10
+b=10
+if a==b:
+    print("equal")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a>b:
+    print("greter")'''
+
+'''a="python"
+if a=="python":
+    print("true")'''
+
+'''a="python"
+if a=="java":
+    print("true")'''
+
+'''a=int(input("value"))
+if a<20:
+    print("true")'''
+
+#if conditions by using logical operators
+#and or not
+'''
+a=4
+b=6
+if a<b and b>a:
+    print("true")'''
+
+'''a=4
+b=6
+if a<=b and b>=a:
+    print("true")'''
+
+
+'''a=4
+b=6
+if a!=b and b==a:
+    print("False")'''
+
+'''a=6
+b=8
+if a<b or b>a:
+    print("true")'''
+
+
+'''a=6
+b=8
+if a>b or b>a:
+    print("true")'''
+
+
+'''a=6
+b=8
+if a<=b or b>=a:
+    print("true")'''
+
+
+'''a=6
+b=8
+if a!=b or b==a:
+    print("true")'''
+
+'''a=int(input("enter a value"))
+b=int(input("enter b value"))
+if a>b and b>a:
+    print("true")'''
+
+'''a=input("data")
+if a=="lakshmi" and a=="bhanu":
+    print("true")'''
+
+#if condition by using Identify operator
+#is is not
+
+'''a=2
+if type(a) is int:
+    print("it is int")'''
+
+'''a=4
+if type(a) is not int:
+    print("it is not int")'''
+
+'''a=int(input("enter a value"))
+if type(a) is int:
+    print("it is int")'''
+
+'''a=int(input("enter a value"))
+if type(a) is  not int:
+    print("it is ot int")'''
+
+
+'''a=2.4
+if type(a) is float:
+    print("it is float")'''
+
+'''a=2.4
+if type(a) is not float:
+    print("it is not float")'''
+
+
+'''a="lakshmi"
+if type(a) is string:
+    print("it is string")'''
+
+'''a="bhanu"
+if type(a) is  not string:
+    print("it is not string")'''
+
+
+'''a=2
+if type(a) is True:
+    print("it is true")'''
+
+#if condition by using Membership operator
+# in, not in
+
+'''a=[1,2,3,4,5,6,7,8]
+if 7 in a:
+    print("true")'''
+
+
+'''a=[1,2,3,4,5,6,7,8]
+if 10 not in a:
+    print("true")'''
+
+'''a=[20,10,40,30,50]
+b=int(input("enter value"))
+if b in a:
+    print("true")'''
+
+
+'''a=[20,10,40,30,50]
+b=int(input("enter value"))
+if b not in a:
+    print("true")'''
+
+#if-else conditions by using comparision operators
+#<,>,<=>=,==,!=
+
+'''a=20
+b=10
+if a<b:
+    print("less")
+else:
+    print("false")'''
+
+'''a=10
+b=30
+if a>b:
+    print("true")
+else:
+    print("false")'''
+
+'''a=10
+b=30
+if b>=a:
+    print("true")
+else:
+    print("false")'''
+
+'''a=10
+b=30
+if b<=a:
+    print("true")
+else:
+    print("false")'''
+
+'''a=10
+b=30
+if b==a:
+    print("true")
+else:
+    print("false")
+
+a=10
+b=30
+if b!=a:
+    print("true")
+else:
+    print("false")'''
+
+#if-else conditions by using Logical operator
+# and or not
+'''a=20
+b=30
+if a>b and b>a:
+    print("true")
+else:
+    print("false")'''
+
+'''a=20
+b=30
+if b>a and a>b:
+    print("true")
+else:
+    print("false")'''
+
+'''a=20
+b=30
+if a>b or b>a:
+    print("true")
+else:
+    print("false")
+
+a=20
+b=30
+if a>b and b>a:
+    print("true")
+else:
+    print("false")'''
+
+'''a=6
+b=9
+if not a>b:
+    print("true")'''
+
+'''a=6
+b=9
+if not b>a:
+    print("true")
+else:
+    print("false")'''
+
+#if-else conditions by using identifier operator
+#is is not
+
+'''a=20
+if type (a) is int:
+    print("is int")
+else:
+  print("is not int")'''
+
+a=1,2,3,4,5,6,7
+if 6 in a:
+    print("true")
+else:
+    print("false")
+
+   
+    
+    
+    
+    
+    
+
+    
+
+
+    
+
+    
+
+
+    
+    
+    
+    
+
+    
+
+
+    
+
+    
