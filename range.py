8981e6fb7c949570283c2eacde565ad4889c1e62
bobbadilakshmi10-cmyk <bobbadilakshmi10@gmail.com>
2026-01-30 11:06:28 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,107 @@
+#range() function:
+#start-stop-step:
+'''for i in range(10):
+    print(i)
+
+for i in range(10,20):
+    print(i)'''
+
+'''for i in range(2,20,2):
+    print(i,end=",")'''
+
+'''for i in range(5,50,5):
+    print(i,end=",")'''
+
+'''for i in range(0,30,3):
+    print(i,end=",")'''
+
+#student Marks()
+'''while True:
+    marks=int(input("enter the marks:"))
+    if marks in range(91,101):
+        print("grade A")
+    elif marks in range(81,91):
+        print("grade B")
+    elif marks in range(71,81):
+        print("grade C")
+    elif marks in range(50,71):
+        print("grade D")
+    else:
+        print("failed")'''
+
+#break:
+'''a=10
+while a>1:
+    print(a)
+    a=a-1
+    if a==7:
+        break'''
+
+'''a=10
+while a>1:
+        a=a-1
+        if a==7:
+             break
+        print(a)
+
+for i in range(20):
+     if i==12:
+        break
+    print(i)
+
+a="python"
+    for i in a:
+    if i=="h":
+        break
+    print(i)'''
+
+#continue:
+'''a=20
+while a>5:
+    print(a)
+    a=a-1
+    if a==10:
+        continue'''
+'''a=20
+while a>5:
+    a=a-1
+    if a==10:
+        continue
+    print(a)'''
+
+'''for i in range(10):
+    if i==7:
+        break
+    print(i)'''
+
+'''a="word"
+for i in a:
+    if i=="r":
+        continue
+    print(i)'''
+
+#pass
+'''a=25
+while a>10:
+    print(a)
+    a=a-1
+    if a==10:
+        pass'''
+
+'''for i in range(30):
+    if i==15:
+        pass
+    print(i)'''
+
+a="world"
+for i in a:
+    if i=="r":
+        pass
+    print(i)
+
+
+    
+
+
+
+
