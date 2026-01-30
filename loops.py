6628de48929f3ae1a804326612b4309f8ad7629e
bobbadilakshmi10-cmyk <bobbadilakshmi10@gmail.com>
2026-01-30 11:06:05 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,140 @@
+#loops:number of iteration
+#for,while,range,break,continue,pass
+#for
+'''a=[1,2,3,4,5,6]
+for i in a:
+    print(i)'''
+
+'''a=[1,2,3,4,5,6]
+for i in a:
+    print(a)
+
+a=[1,2,3,4,5,6]
+for i in a:
+    print(i,end=" ")'''
+
+'''a=[1,2,3,4,5,6]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+
+a=(1,2,3,4,5,6)
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+
+a={1,2,3,4,5,6}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a={"name":"lakshmi","year":2026,"month":1}
+for i in a:
+    print(i)
+for i in a.keys():
+    print(i)
+    print(type(a))
+    print(type(i))
+for i in a.values():
+    print(type(a))
+    print(type(i))
+for i in a.items():
+    print(type(a))
+    print(type(i))'''
+
+'''fruits=["apple","bannan","kiwi"]
+for i in fruits:
+    print(i)
+    print(i,end=" ")
+    print(type(fruits))
+    print(type(i))'''
+
+#float:
+'''a=[3.5,2.5,6.8,7.9]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+#boolean
+'''a=[5+2j,True]
+for i in a:
+  print(i)
+  print(type(a))
+  print(type(i))'''
+
+'''a=[3,4.5,"python",3+8j,True,False]
+for i in a:
+  print(i)
+  print(type(a))
+  print(type(i))'''
+
+'''a=["python","java","html"]
+for i in a:
+    print(i.title(),end=" ")
+
+b=str(a)
+c=b.title()
+print(c)'''
+
+#while loop:
+'''a=10
+while a>1:
+    print(a)'''
+
+'''a=10
+while a>1:
+    print(a)
+    a=a-1'''
+
+'''a=10
+while a>1:
+    a=a-1
+    print(a)'''
+
+'''a=10
+while a>1:
+print(a)
+    a=a-1'''#Syntax error
+
+'''a=10
+while a>1:
+    print(a)
+    a+=1'''#infinity
+
+'''a=1
+while a<10:
+    print(a)
+    a+=1'''
+
+'''a=20
+while a>1:
+    print(a)
+    a-=1'''
+
+'''while True:
+    name=input("username")
+    password=input("password")
+    if name=="lakshmi":
+        if password=="1234":
+            print("sucess")
+else:
+    print("not sucess")'''
+
+'''while True:
+    age=int(input("enter the age"))
+    if age>=18:
+        print("elgible for vote")
+else:
+    print("not elgible")'''
+
+
+
+
+    
+
+    
+
