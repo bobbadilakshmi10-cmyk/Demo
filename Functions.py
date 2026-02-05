3c52701aabf667fb68221f50fbc310f701c39085
bobbadilakshmi10-cmyk <bobbadilakshmi10@gmail.com>
2026-02-05 14:53:56 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,128 @@
+#functions:
+'''a=10
+b=20
+print("the sum is",a+b)
+print("the diff is",a-b)
+print("the product is",a*b)
+a=100
+b=200
+print("the sum is",a+b)
+print("the diff is",a-b)
+print("the product is",a*b)
+a=1000
+b=2000
+print("the sum is",a+b)
+print("the diff is",a-b)
+print("the product is",a*b)'''
+
+'''def calculate(a,b):
+    print("the sum is",a+b)
+    print("the diff is",a-b)
+    print("the product is",a*b)
+calculate(10,20)
+calculate(100,200)
+calculate(1000,2000)'''
+'''a=10
+b=20
+print("the power is",a^b)
+print("integer division is",a//b)
+print("the modules is",a%b)
+a=100
+b=200
+print("the power is",a^b)
+print("integer division is",a//b)
+print("the modules is",a%b)
+a=1000
+b=2000
+print("the power is",a^b)
+print("integer division is",a//b)
+print("the modules is",a%b)'''
+
+'''def calculate1(a,b):
+    print("the power is",a**b)
+    print("integer division is",a/b)
+    print("the modules is",a%b)
+    print("the flot division",a//b)
+calculate1(10,20)
+calculate1(2,4
+calculate1(5,4)'''
+
+'''def add(a,b):
+    c=a+b
+    print(c)
+add(6,7)
+
+while True:
+    def add():
+        a=int(input("a value"))
+        b=int(input("b value"))
+        print(a+b)
+    add()
+
+#recursive function:
+    def add():
+        a=int(input("a value"))
+        b=int(input("b value"))
+        print(a+b)
+    add()
+add()'''
+
+'''def fullname():
+    fname=input("first name")
+    lname=input("last name")
+    print(fname+" "+lname)
+fullname()
+fullname()#recursive function'''
+
+'''def cal(a,b):
+    print(a*b)
+cal(4,3)
+
+def cal(a,b):
+    return a*b
+print(cal(5,4))'''
+
+#print v/s return:
+'''def call(a,b):
+    c=a+b
+    d=a-b
+    e=a*b
+    print(c)
+    print(d)
+    print(c)
+call(4,5)
+
+def call(a,b):
+    c=a+b
+    d=a-b
+    e=a*b
+    print(c)
+    print(d)
+    print(c)
+print(call(4,5))'''
+
+'''def cal(a,b):
+    c=a+b
+    d=a-b
+    e=a*b
+    #return c
+    #return d
+    #return e
+    return c,d,e
+print(cal(4,5))'''
+
+#Atm Applications:
+account=100000
+pwd=1234
+card=input("insert the card")
+if card=="c":
+    print("welcome lakshmi")
+    password==int(input("enter the password")
+        if password==pwd:
+                  
+
+   
+
+
+    
+
