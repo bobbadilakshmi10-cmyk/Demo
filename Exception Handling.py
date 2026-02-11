0fce5f5898fb0d97094052d418aa922cd3133f0d
bobbadilakshmi10-cmyk <bobbadilakshmi10@gmail.com>
2026-02-11 19:33:59 +0530

    Add files via upload

---
+++ Exception Handling.py
@@ -0,0 +1,24 @@
+#Exception Handling
+'''a=int(input("a value:"))
+b=int(input("b value:"))
+try:
+    c=a//b
+    print(c)
+except:
+    print("exception is raised")
+else:
+    print("no_exception")
+finally:
+    print("ends program")
+
+'''Try blocked is correct
+a value:12
+b value:2
+6
+no_exception
+ends program'''
+
+a value:12
+b value:0
+exception is raised
+ends program'''
