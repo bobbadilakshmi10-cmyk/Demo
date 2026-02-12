cb1bd550d51f48125238ce31a89d95ff8459c9cf
bobbadilakshmi10-cmyk <bobbadilakshmi10@gmail.com>
2026-02-12 18:22:10 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,19 @@
+'''import mymodules
+mymodules.greetings("lakshmi")'''
+
+'''import mymodules
+
+the sum is: 9'''
+
+'''import mymodules
+a=mymodules.details["names"]
+b=mymodules.details["places"]
+print(a)
+print(b)
+
+['lakshmi', 'bhanu', 'sudha']
+['vij', 'vzg', 'hyd']'''
+
+import mymodules
+
+this program is run as module
